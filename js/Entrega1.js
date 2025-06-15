40678d6a213bc4de38397022419bc70565abef7d
Dedecow <andrehbahia@gmail.com>
2025-06-15 06:45:44 -0300

    arrumei-exibir-param

--- js/Entrega1.js
+++ js/Entrega1.js
@@ -98,8 +98,8 @@ class Investimento {
 
 const parametroPoupanca = new Investimento("Poupança", 0.0500, "nao");
 const parametroPetro = new Investimento("Petr4", 0.2707, "sim")
-parametroPoupanca.exibirInfo()
-parametroPetro.exibirInfo()
+console.log(parametroPoupanca.exibirInfo())
+console.log(parametroPetro.exibirInfo())
 
 
 //Chamada das funções referentes à informações do usuário.
